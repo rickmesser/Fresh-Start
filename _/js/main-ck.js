3b8d88e4c2ab04ebb2b66d701178aa4fa363ff67
rick <rick@rickmesser.com>
2013-04-10 16:21:19 -0500

    Add HAML & Modernizr

---
+++ _/js/main-ck.js
@@ -0,0 +1,2 @@
+// Initiate Carousel Automatically
+$(".carousel").carousel({interval:4500});$("#brand a").bind("click",function(e){e.preventDefault();$("html, body").animate({scrollTop:$(this.hash).offset().top},1e3)});
